2c0d8673798bd8e92a4df5385660358fee05c186
DrutskoKiryl <bitbtn@yandex.ru>
2023-09-20 19:18:32 +0300

    v4

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import * as THREE from 'three';
 document.querySelector('.btn').addEventListener( 'click',  activateXR)
 
 async function activateXR() {
-    // Add a canvas element and initialize a WebGL context that is compatible with WebXR.
+     // Add a canvas element and initialize a WebGL context that is compatible with WebXR.
     const canvas = document.createElement("canvas");
     document.body.appendChild(canvas);
     const gl = canvas.getContext("webgl", {xrCompatible: true});
@@ -26,7 +26,6 @@ async function activateXR() {
     cube.position.set(1, 1, 1);
     scene.add(cube);
 
-
     // Set up the WebGLRenderer, which handles rendering to the session's base layer.
     const renderer = new THREE.WebGLRenderer({
         alpha: true,
@@ -35,13 +34,12 @@ async function activateXR() {
         context: gl
     });
     renderer.autoClear = false;
-
+    
     // The API directly updates the camera matrices.
     // Disable matrix auto updates so three.js doesn't attempt
     // to handle the matrices independently.
     const camera = new THREE.PerspectiveCamera();
     camera.matrixAutoUpdate = false;
-
 
     // Initialize a WebXR session using "immersive-ar".
     const session = await navigator.xr.requestSession("immersive-ar");
@@ -53,34 +51,34 @@ async function activateXR() {
     // near the viewer's position at the time the session was created.
     const referenceSpace = await session.requestReferenceSpace('local');
 
-
     // Create a render loop that allows us to draw on the AR view.
     const onXRFrame = (time, frame) => {
         // Queue up the next draw request.
         session.requestAnimationFrame(onXRFrame);
-
+    
         // Bind the graphics framebuffer to the baseLayer's framebuffer
         gl.bindFramebuffer(gl.FRAMEBUFFER, session.renderState.baseLayer.framebuffer)
-
+    
         // Retrieve the pose of the device.
         // XRFrame.getViewerPose can return null while the session attempts to establish tracking.
         const pose = frame.getViewerPose(referenceSpace);
         if (pose) {
-            // In mobile AR, we only have one view.
-            const view = pose.views[0];
-
-            const viewport = session.renderState.baseLayer.getViewport(view);
-            renderer.setSize(viewport.width, viewport.height)
-
-            // Use the view's transform matrix and projection matrix to configure the THREE.camera.
-            camera.matrix.fromArray(view.transform.matrix)
-            camera.projectionMatrix.fromArray(view.projectionMatrix);
-            camera.updateMatrixWorld(true);
-
-            // Render the scene with THREE.WebGLRenderer.
-            renderer.render(scene, camera)
+        // In mobile AR, we only have one view.
+        const view = pose.views[0];
+    
+        const viewport = session.renderState.baseLayer.getViewport(view);
+        renderer.setSize(viewport.width, viewport.height)
+    
+        // Use the view's transform matrix and projection matrix to configure the THREE.camera.
+        camera.matrix.fromArray(view.transform.matrix)
+        camera.projectionMatrix.fromArray(view.projectionMatrix);
+        camera.updateMatrixWorld(true);
+    
+        // Render the scene with THREE.WebGLRenderer.
+        renderer.render(scene, camera)
         }
     }
-    session.requestAnimationFrame(onXRFrame);
     
+    session.requestAnimationFrame(onXRFrame);
+        
 }
